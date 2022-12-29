6288ff5b10e0136045cf0749374bc1aa71692601
vlmnst <vale-mansueto@hotmail.com>
2022-12-29 11:57:36 -0300

    import de logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,8 @@
+import logo from "../assets/sinfondo.png"
 function Nav() {
   return (
     <nav className="flex sm:flex-row sm:justify-between  bg-slate-700 p-5 items-center flex-col">
-      <img src="../src/assets/sinfondo.png" alt="logo" width="200" className="h-auto " />
+      <img src={logo} alt="logo" width="200" className="h-auto " />
 
       <div className="flex flex-col sm:flex-row sm:justify-between justify-center text-white m-5 items-center text-sm uppercase">
         <div className="mt-2 sm:mr-3 sm:ml-3">
